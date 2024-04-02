5e4859f6b7e1feb566dfb3264c62cc965094a96c
jaspi <jaspindersingh.maan@ucalgary.ca>
2024-04-02 14:01:07 -0600

    made some changes in Writer

--- src/main/java/rw/util/Writer.java
+++ src/main/java/rw/util/Writer.java
@@ -15,10 +15,9 @@ public class Writer {
 
     public static void saveBattle(Battle battle, File file) throws IOException {
         try (PrintWriter pw = new PrintWriter(new FileWriter(file))) {
-            // Assuming Battle class has methods to get its dimensions
             int rows = battle.getRows();
             int columns = battle.getColumns();
-            pw.println(rows + " " + columns);
+            pw.println(rows + "\n" + columns);
 
             // Iterate over each position in the battle grid
             for (int i = 0; i < rows; i++) {
@@ -35,21 +34,19 @@ public class Writer {
         } // PrintWriter is auto-closed here, thanks to try-with-resources
     }
 
+
     private static String entityToLine(Entity entity, int row, int column) {
-        // Convert an entity and its position to a string line for saving
         if (entity instanceof Maximal) {
             Maximal maximal = (Maximal) entity;
-            // Assuming Maximal has getters for its properties
-            return String.format("%d%dM%c%s%d%d%d", row, column, maximal.getSymbol(),
+
+            return String.format("%d,%d,MAXIMAL,%c,%s,%d,%d,%d", row, column, maximal.getSymbol(),
                     maximal.getName(), maximal.getHealth(), maximal.weaponStrength(), maximal.armorStrength());
         } else if (entity instanceof PredaCon) {
             PredaCon predaCon = (PredaCon) entity;
-            // Assuming PredaCon has getters for its properties
-            return String.format("%d%dP%c%s%d%s", row, column, predaCon.getSymbol(),
+            return String.format("%d,%d,PREDACON,%c,%s,%d,%s", row, column, predaCon.getSymbol(),
                     predaCon.getName(), predaCon.getHealth(), predaCon.getWeaponType().name());
         } else if (entity instanceof Wall) {
-            // Walls could be represented differently depending on your needs
-            return String.format("%d%dW", row, column);
+            return String.format("%d,%d,WALL", row, column);
         }
         // Return null if it's an unknown entity or empty space
         return null;
