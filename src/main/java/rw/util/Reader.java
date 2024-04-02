deaaf0ce3e47f1b11db4af69edf38c3474c2c11b
jaspi <jaspindersingh.maan@ucalgary.ca>
2024-04-01 21:00:17 -0600

    made some changes in reader

--- src/main/java/rw/util/Reader.java
+++ src/main/java/rw/util/Reader.java
@@ -3,6 +3,8 @@ package rw.util;
 import rw.battle.Battle;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Class to assist reading in battle file
@@ -11,9 +13,31 @@ import java.io.File;
  * @version 1.0
  */
 public final class Reader {
-//for the inital commit
-    //for test commit 2
+
     public static Battle loadBattle(File file) {
-        return null;
+        Battle battle=null;
+        try(Scanner scanner=new Scanner(file)){
+            int rows= scanner.nextInt();
+            int col=scanner.nextInt();
+            battle=new Battle(rows,col);
+
+            while (scanner.hasNextLine()){
+                String line=scanner.nextLine().trim();
+                if(!line.isEmpty()){
+                    String[] parts=line.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+                    int row = Integer.parseInt(parts[0]);
+                    int column=Integer.parseInt(parts[1]);
+                    String type=parts[2];
+                    String name=parts[3];
+                    int Health=Integer.parseInt(parts[4]);
+                    char weaponType=parts[4].charAt(0);
+                }
+            }
+            return null;
+        }
+        catch (FileNotFoundException e){
+            System.err.println("File not found: "+file.getPath());
+            return null;
+        }
     }
 }
